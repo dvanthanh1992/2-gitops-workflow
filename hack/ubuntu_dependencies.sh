ac3d5fd5cfa70776dcc2f6cbb35e4f1a4baf483e
thanh.duong <106790691+thanh-duong-iij@users.noreply.github.com>
2025-03-21 10:10:12 +0700

    only u24

--- hack/ubuntu_dependencies.sh
+++ hack/ubuntu_dependencies.sh
@@ -13,8 +13,8 @@ sudo apt-get update -y
 
 echo "🔹 Installing required packages..."
 sudo apt-get install -y curl wget unzip tar python3 python3-pip python3-apt \
-    apt-transport-https ca-certificates software-properties-common git gh \
-    direnv sshpass vim rsync openssh-client jq yq xorriso apache2-utils
+    apt-transport-https ca-certificates software-properties-common git gh tree \
+    direnv sshpass vim rsync openssh-client jq yq xorriso apache2-utils dos2unix 
 
 echo "🔹 Adding Docker repository..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
